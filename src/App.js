15daeda9a9f3a9aafc2807ebfe06dc10a5c8ebab
Javiera Hidalgo <javiera_56@hotmail.com>
2022-03-20 16:54:16 -0300

    added initial structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App
-
+    <div className="App"> 
+    Weather App
+      <footer>
+        <a href="https://github.com/javytaxime14/react-weather-app" target="_blank" rel="noreferrer">
+        Open-sourced project on GitHub</a>, coded by <a href="https://www.linkedin.com/in/javiera-hidalgo-n%C3%BA%C3%B1ez-87101022a/" target="_blank" rel="noreferrer">Javiera Hidalgo</a> 🦋
+      </footer>
     </div>
   );
 }
